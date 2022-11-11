0cbeee6f1cc6159c7a08fdbbd0842a89820ac5c1
AnilBamne <anilbamne20@gmail.com>
2022-11-11 17:45:13 +0530

    Adding Welcome to snake and ladder prolem message

---
+++ SnakeAndLadderProblem222Batch/SnakeAndLadder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem222Batch
+{
+    public class SnakeAndLadder
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Program");
+        }
+    }
+}
