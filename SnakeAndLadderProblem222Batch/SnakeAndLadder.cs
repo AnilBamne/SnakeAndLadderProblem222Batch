220b597108b893bd8c831eb698c009e96cf40382
AnilBamne <anilbamne20@gmail.com>
2022-11-12 09:05:11 +0530

    Adding UC5PlayerGetsExact100Position Code

--- SnakeAndLadderProblem222Batch/SnakeAndLadder.cs
+++ SnakeAndLadderProblem222Batch/SnakeAndLadder.cs
@@ -13,40 +13,57 @@ namespace SnakeAndLadderProblem222Batch
             Console.WriteLine("Welcome To Snake And Ladder Program");
             //uc1 : Game played by single player
             int Position = 0;
+            //uc4,uc5 : ensure player gets exact 100 position
             int FinalPosition = 100;
             Random random = new Random();
             //uc2 : Player rolls a die to get between 1-6
-            while(Position <= FinalPosition)
-            {
-                int die = random.Next(1, 7);
-                Console.WriteLine("Die rolled for : " + die);
-                Position += die;
-                Console.WriteLine("Player position is : " + Position);
-                //uc3 : Player checks for option LADDER or SNAKE
-                int option = random.Next(0, 3);
-                switch (option)
-                {
-                    case 2:
-                        Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
-                        Position += die;
-                        break;
-                    case 1:
-                        Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
-                        Position -= die;
-                        break;
 
-                    default:
-                        Console.WriteLine("No Play");
+            int die = random.Next(1, 7);
+            Console.WriteLine("Die rolled for : " + die);
+            Position += die;
+            while (Position <= FinalPosition)
+            {
+                  die = random.Next(1, 7);
+                  Console.WriteLine("Die rolled for : " + die);
+                  Position += die;
+                  if (Position > FinalPosition)
+                  {
+                     Position = Position - die;
+                  }
+                  Console.WriteLine("Player position is : " + Position);
+                  //uc3 : Player checks for option LADDER or SNAKE
+                  int option = random.Next(0, 3);
+                  switch (option)
+                  {
+                        case 2:
+                            Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
+                            Position += die;
+                            if (Position > FinalPosition)
+                            {
+                                Position = Position - die;
+                            }
                         break;
+                        case 1:
+                            Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
+                            Position -= die;
+                            break;
+
+                        default:
+                            Console.WriteLine("No Play");
+                            break;
+                  }
+                //UC4 , UC5 : get exact 100 position
+                if(Position == FinalPosition)
+                {
+                    Console.WriteLine("Player position is : " + Position);
+                    break;
                 }
                 if (Position < 0)
                 {
                     Position = 0;
                 }
-                Console.WriteLine("Player position is : " + Position);
             }
-           
-            Console.WriteLine("Player position is : "+ Position);
+            Console.WriteLine("Players Final  position is : "+ Position);
         }
     }
 }
