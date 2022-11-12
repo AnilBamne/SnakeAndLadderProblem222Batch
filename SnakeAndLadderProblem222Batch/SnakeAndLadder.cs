4fde4a465a5a5ef542be610ddb4b15756781ab84
AnilBamne <anilbamne20@gmail.com>
2022-11-12 08:21:13 +0530

    Adding UC4RepeatTillPlayerReach100Position Code

--- SnakeAndLadderProblem222Batch/SnakeAndLadder.cs
+++ SnakeAndLadderProblem222Batch/SnakeAndLadder.cs
@@ -13,31 +13,40 @@ namespace SnakeAndLadderProblem222Batch
             Console.WriteLine("Welcome To Snake And Ladder Program");
             //uc1 : Game played by single player
             int Position = 0;
+            int FinalPosition = 100;
             Random random = new Random();
             //uc2 : Player rolls a die to get between 1-6
-            int die=random.Next(1,7);
-            Console.WriteLine("Die rolled for : "+die);
-            Position += die;
-            Console.WriteLine("Player position is : " + Position);
-            //uc3 : Player checks for option LADDER or SNAKE
-            int option = random.Next(0, 3);
-            switch (option)
+            while(Position <= FinalPosition)
             {
-                case 2:
-                    Console.WriteLine("Player got Ladder Next position : {0}+{1}",Position,die);
-                    Position += die;
-                    break;
-                case 1:
-                    Console.WriteLine("Player got Snake Next position : {0}-{1}", Position,die);
-                    Position -= die;
-                    break;
+                int die = random.Next(1, 7);
+                Console.WriteLine("Die rolled for : " + die);
+                Position += die;
+                Console.WriteLine("Player position is : " + Position);
+                //uc3 : Player checks for option LADDER or SNAKE
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case 2:
+                        Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
+                        Position += die;
+                        break;
+                    case 1:
+                        Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
+                        Position -= die;
+                        break;
 
-                default:
-                    Console.WriteLine("No Play");
-                    break;
-
+                    default:
+                        Console.WriteLine("No Play");
+                        break;
+                }
+                if (Position < 0)
+                {
+                    Position = 0;
+                }
+                Console.WriteLine("Player position is : " + Position);
             }
-            Console.WriteLine("Player position is : "+Position);
+           
+            Console.WriteLine("Player position is : "+ Position);
         }
     }
 }
