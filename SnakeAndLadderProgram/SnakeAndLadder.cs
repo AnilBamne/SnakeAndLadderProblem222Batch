c72995b477aadd09236eada7a732edabac2dbb94
AnilBamne <anilbamne20@gmail.com>
2022-11-25 14:45:20 +0530

    Adding UC3CheckForOptionLadderOrSnake code

--- SnakeAndLadderProgram/SnakeAndLadder.cs
+++ SnakeAndLadderProgram/SnakeAndLadder.cs
@@ -16,8 +16,29 @@ namespace SnakeAndLadderProgram
             Random random = new Random();
             //uc2 : Player rolls a die to get between 1-6
             int die = random.Next(1, 7);
+            Console.WriteLine("Die rolled for : " + die);
             Position += die;
+            Console.WriteLine("Player position is : " + Position);
+            //uc3 : Player checks for option LADDER or SNAKE
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case 2:
+                    Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
+                    Position += die;
+                    break;
+                case 1:
+                    Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
+                    Position -= die;
+                    break;
+
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+
+            }
             Console.WriteLine("Player position is : " + Position);
         }
     }
+    
 }
