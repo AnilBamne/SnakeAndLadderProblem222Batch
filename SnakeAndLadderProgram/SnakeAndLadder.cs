ff31dc6d76c372f8b9243c0ce42c1e8a2ff11df7
AnilBamne <anilbamne20@gmail.com>
2022-11-25 14:35:33 +0530

    Welcome message

---
+++ SnakeAndLadderProgram/SnakeAndLadder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    public class SnakeAndLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder program");
+        }
+    }
+}
