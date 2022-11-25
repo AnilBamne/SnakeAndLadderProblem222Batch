77853d9082c558b71b0289f28df5395de99a3882
AnilBamne <anilbamne20@gmail.com>
2022-11-25 13:31:53 +0530

    Modified UC5PlayerGetsExact100Position code,removed unwanted spaces

--- SnakeAndLadderProblem222Batch/SnakeAndLadder.cs
+++ SnakeAndLadderProblem222Batch/SnakeAndLadder.cs
@@ -16,51 +16,41 @@ namespace SnakeAndLadderProblem222Batch
             //uc4,uc5 : ensure player gets exact 100 position
             int FinalPosition = 100;
             Random random = new Random();
-            //uc2 : Player rolls a die to get between 1-6
-
-            int die = random.Next(1, 7);
-            Console.WriteLine("Die rolled for : " + die);
-            Position += die;
             while (Position <= FinalPosition)
             {
-                  die = random.Next(1, 7);
-                  Console.WriteLine("Die rolled for : " + die);
-                  Position += die;
-                  if (Position > FinalPosition)
-                  {
-                     Position = Position - die;
-                  }
-                  Console.WriteLine("Player position is : " + Position);
-                  //uc3 : Player checks for option LADDER or SNAKE
-                  int option = random.Next(0, 3);
-                  switch (option)
-                  {
-                        case 2:
-                            Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
-                            Position += die;
-                            if (Position > FinalPosition)
-                            {
-                                Position = Position - die;
-                            }
+                int die = random.Next(1, 7);
+                Console.WriteLine("Player position is : " + Position);
+                //uc3 : Player checks for option LADDER or SNAKE
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case 2:
+                        Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
+                        Position += die;
+                        if (Position > FinalPosition)
+                        {
+                            Position = Position - die;
+                        }
                         break;
-                        case 1:
-                            Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
-                            Position -= die;
-                            break;
-
-                        default:
-                            Console.WriteLine("No Play");
-                            break;
-                  }
+                    case 1:
+                        Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
+                        Position -= die;
+                        if (Position < 0)
+                        {
+                            Position = 0;
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("No Play");
+                        break;
+                    default:
+                        break;
+                }
                 //UC4 , UC5 : get exact 100 position
-                if(Position == FinalPosition)
+                if (Position == FinalPosition)
                 {
                     Console.WriteLine("Player position is : " + Position);
                     break;
-                }
-                if (Position < 0)
-                {
-                    Position = 0;
                 }
             }
             Console.WriteLine("Players Final  position is : "+ Position);
