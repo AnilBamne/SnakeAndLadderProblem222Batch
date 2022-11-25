9607809fd33242a88778cde311c63da64dfb1617
AnilBamne <anilbamne20@gmail.com>
2022-11-25 14:53:55 +0530

    Removing Unwanted spaces in UC5EnsureExact100Position

--- SnakeAndLadderProgram/SnakeAndLadder.cs
+++ SnakeAndLadderProgram/SnakeAndLadder.cs
@@ -10,42 +10,52 @@ namespace SnakeAndLadderProgram
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome To Snake And Ladder Program");
-            //uc1 : Game played by single player
-            int Position = 0;
-            int FinalPosition = 100;
-            Random random = new Random();
-            //uc2 : Player rolls a die to get between 1-6
-            while (Position <= FinalPosition)
             {
-                int die = random.Next(1, 7);
-                Console.WriteLine("Die rolled for : " + die);
-                Position += die;
-                Console.WriteLine("Player position is : " + Position);
-                //uc3 : Player checks for option LADDER or SNAKE
-                int option = random.Next(0, 3);
-                switch (option)
+                Console.WriteLine("Welcome To Snake And Ladder Program");
+                //uc1 : Game played by single player
+                int Position = 0;
+                //uc4,uc5 : ensure player gets exact 100 position
+                int FinalPosition = 100;
+                Random random = new Random();
+                while (Position <= FinalPosition)
                 {
-                    case 2:
-                        Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
-                        Position += die;
+                    int die = random.Next(1, 7);
+                    Console.WriteLine("Player position is : " + Position);
+                    //uc3 : Player checks for option LADDER or SNAKE
+                    int option = random.Next(0, 3);
+                    switch (option)
+                    {
+                        case 2:
+                            Console.WriteLine("Player got Ladder Next position : {0}+{1}", Position, die);
+                            Position += die;
+                            if (Position > FinalPosition)
+                            {
+                                Position = Position - die;
+                            }
+                            break;
+                        case 1:
+                            Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
+                            Position -= die;
+                            if (Position < 0)
+                            {
+                                Position = 0;
+                            }
+                            break;
+                        case 0:
+                            Console.WriteLine("No Play");
+                            break;
+                        default:
+                            break;
+                    }
+                    //UC4 , UC5 : get exact 100 position
+                    if (Position == FinalPosition)
+                    {
+                        Console.WriteLine("Player position is : " + Position);
                         break;
-                    case 1:
-                        Console.WriteLine("Player got Snake Next position : {0}-{1}", Position, die);
-                        Position -= die;
-                        break;
-
-                    default:
-                        Console.WriteLine("No Play");
-                        break;
+                    }
                 }
-                if (Position < 0)
-                {
-                    Position = 0;
-                }
-                Console.WriteLine("Player position is : " + Position);
+                Console.WriteLine("Players Final  position is : " + Position);
             }
-            Console.WriteLine("Player position is : " + Position);
         }
     }
     
