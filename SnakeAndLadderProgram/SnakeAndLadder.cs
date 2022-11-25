af1b8c90077d00ae28dd554fe8be4ba6d2797dcf
AnilBamne <anilbamne20@gmail.com>
2022-11-25 14:42:09 +0530

    Adding UC2PlayerRollesADie code

--- SnakeAndLadderProgram/SnakeAndLadder.cs
+++ SnakeAndLadderProgram/SnakeAndLadder.cs
@@ -11,9 +11,12 @@ namespace SnakeAndLadderProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Program");
-            //uc1
-            //Game played by single player
+            //uc1 : Game played by single player
             int Position = 0;
+            Random random = new Random();
+            //uc2 : Player rolls a die to get between 1-6
+            int die = random.Next(1, 7);
+            Position += die;
             Console.WriteLine("Player position is : " + Position);
         }
     }
